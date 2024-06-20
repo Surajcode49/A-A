e9b0a83b6633c4590d2a3c0519857d20ed9e2735
krish <krishabotadara@gmail.com>
2024-06-20 15:15:13 +0700

    new

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -87,7 +87,7 @@ export default function Footer() {
                     <h3 className="text-black font-bold text-lg mb-4 roboto-light">COMPANY</h3>
                     <ul>
                         <motion.li className="mb-2" whileHover={{ scale: 1.1 }}>
-                            <a href="#" className="text-accent roboto-medium hover:text-[#8D8B87]">Homeeeeeeeeee</a>
+                            <a href="#" className="text-accent roboto-medium hover:text-[#8D8B87]">Home</a>
                         </motion.li>
                         <motion.li className="mb-2" whileHover={{ scale: 1.1 }}>
                             <a href="#" className="text-accent roboto-medium hover:text-[#8D8B87]">About Us</a>
