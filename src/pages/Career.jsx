cab38c06ebca1acabfadaa1f06b5831941b0c07c
Suraj kumar <devilsuraj9641@gmail.com>
2024-06-21 00:47:37 +0530

    ye bhai solve hoo ja

---
+++ src/pages/Career.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+const Career = () => {
+  return (
+    <div>
+      <h1>Career Page</h1>
+      {/* Page content */}
+    </div>
+  );
+};
+
+export default Career;
