5f4e0dd9f7b7676124538da3ffd00a115ce6c4cb
krish <krishabotadara@gmail.com>
2024-07-24 22:20:57 +0700

    Updated gradient headings

--- src/components/SectionHeading.jsx
+++ src/components/SectionHeading.jsx
@@ -26,7 +26,7 @@ export default function SectionHeading({ Title, heading }) {
         <div>
             <p className="text-black text-md font-semibold text-nowrap xllll:text-xl xlllll:text-[2rem] roboto-light">{Title}</p>
            
-                <h2 className="text-transparent bg-clip-text bg-gradient-to-r from-main3 to-main1 lg:text-[1.5rem] xlllll:text-[2.5rem] roboto-black xlllll:mt-5 xllll:text-3xl sssm:text-lg md:text-[1.2rem] font-bold">
+                <h2 className="text-transparent bg-clip-text bg-gradient-to-r from-[#03c6e6] to-[#2388bb] from-1% to-99% lg:text-[1.5rem] xlllll:text-[2.5rem] roboto-black xlllll:mt-5 xllll:text-3xl sssm:text-lg md:text-[1.2rem] font-bold">
                 {heading} 
                 </h2>
         </div>
