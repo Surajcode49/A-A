8b6ad1ebf2510578a7261fa270a2fddd99f28fec
krish <krishabotadara@gmail.com>
2024-07-29 12:44:03 +0700

    updated routes in service section of homepage

--- src/components/Services/service.jsx
+++ src/components/Services/service.jsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
 import { useState } from "react";
 import { motion } from "framer-motion";
-import Link from 'next/link';
 import SectionHeadingNew from "../SectionHeadingNew";
 import ReadButton from "../ReadButton";
+import Link from "next/link";
 
 /*Details for each service card*/
 const slides = [
@@ -58,6 +58,11 @@ export default function Service() {
         setCurrentIndex((prevIndex) => (prevIndex - 1 + slides.length) % slides.length);
     };
 
+    /*Click handler to change slide*/
+    const handleHeadingClick = (index) => {
+        setCurrentIndex(index);
+    };
+
     return (
         <div className="md:flex sssm:block space-x-30 lg:mt-[10vh] sssm:mt-[5vh] justify-items-center items-center mx-[10vw] md:mx-[5vw] relative">
             <div className="w-full">
@@ -67,49 +72,51 @@ export default function Service() {
                 {/*Updating the color of bulletin text based on card displayed*/}
                 <ul className="text-nowrap lg:leading-10 lgg:mt-7 xlllll:mt-10 font-sans sssm:leading-[3vh] lg:text-xl xllll:leading-[8vh] xlllll:leading-[6vh] xlllll:text-[2rem] xllll:text-2xl md:text-lg sssm:text-xs px-4">
                     {slides.map((slide, index) => (
-                        <Link href={slide.link} key={index} passHref>
-                            <motion.div
-                                className="flex flex-nowrap cursor-pointer"
-                                initial={{ x: 0 }}
-                                whileHover={{ x: 20 }}
-                                transition={{
-                                    duration: 0.7,
-                                    ease: "easeInOut",
-                                }}
-                            >
-                                <div className={`${
-                                    index === currentIndex ? "bg-accent font-bold" : "bg-main2"
-                                } xxxxxxl:my-14 xlllll:my-4 xlllll:w-10 xlllll:h-2 sssm:w-5 sssm:h-1.3 sssm:my-2 xl:my-2 xl:mt-5 xlllll:mt-8 w-7 h-1.5 rounded-sm mr-5`}>
-                                </div>
+                        <div>
+                        <motion.div
+                            key={index}
+                            className="flex flex-nowrap cursor-pointer"
+                            initial={{ x: 0 }}
+                            whileHover={{ x: 20 }}
+                            transition={{
+                                duration: 0.7,
+                                ease: "easeInOut",
+                            }}
+                            onClick={() => handleHeadingClick(index)}
+                        >
+                            <div className={`${
+                                index === currentIndex ? "bg-accent font-bold" : "bg-main2"
+                            } xxxxxxl:my-14 xlllll:my-4 xlllll:w-10 xlllll:h-2 sssm:w-5 sssm:h-1.3 sssm:my-2 xl:my-2 xl:mt-5 xlllll:mt-8 w-7 h-1.5 rounded-sm mr-5`}>
+                            </div>
 
-                                <li className={`${
-                                    index === currentIndex ? "text-accent font-bold" : "text-main2 font-semibold"
-                                }`}>
-                                    {slide.bulletText}
-                                </li>
-                            </motion.div>
-
-                             {/*Updating the counter value of services*/}
-                             <h2 className="service absolute roboto-black xlllll:top-[67.5vh]  xxl:top-[60vh] xll:top-[55vh] lgg:top-[75vh] lg:top-[75vh] xl:top-[81vh] xl:left-[0.8vw] sssm:left-[63vw] sssm:top-[15vh] md:top-[10vh] font-bold lg:left-10 sssm:right-10 text-main3 lg:text-[10rem] sssm:text-[4rem] md:text-[7rem] xlllll:text-[14rem]">{currentIndex + 1}</h2>
-                            <h2 className="service absolute roboto-black xlllll:top-[67vh]  xlx:left-[3vw] xxl:top-[59vh] xll:top-[54vh] lgg:top-[74vh] lgg:left-[3.5vw] lg:top-[75vh] xl:top-[80vh] xl:-ml-8 xlllll:-ml-7 sssm:top-[14.7vh] sssm:left-[62.2vw] md:top-[10vh] font-bold xllll:left-[2vw] lg:left-[3vw] sssm:right-[11.5vw] ssmm:right-[10.4vw] ssm:right-[10vw] md:right-[5.5vw] md:text-[7rem] text-gray-200 lg:text-[10rem] sssm:text-[4rem] xlllll:text-[14rem] -z-1">{currentIndex + 1}</h2>
-                            <hr className="absolute xlllll:top-[82vh]  xxl:top-[73vh] xll:top-[65vh] lxl:left-[3vw] xl:top-[98vh] lgg:top-[90vh] lg:top-[95vh] sssm:top-[18vh] sssm:left-[65vw] xl:left-[2vw] xlllll:left-[1.5vw] ssmm:top-[22vh] smmm:top-[21vh] md:top-[25vh] lgg:left-[5vw] lg:left-[4vw] sssm:right-9 lg:border-2 sssm:border-1 border-gray-300 w-[7vw] xlllll:w-[6vw]"/>
-
-                        </Link>
+                            <li className={`${
+                                index === currentIndex ? "text-accent font-bold" : "text-main2 font-semibold"
+                            }`}>
+                                {slide.bulletText}
+                            </li>
+                        </motion.div>
+                        <h2 className="service absolute roboto-black xlllll:top-[67.5vh]  xxl:top-[60vh] xll:top-[55vh] lgg:top-[75vh] lg:top-[75vh] xl:top-[81vh] xl:left-[0.8vw] sssm:left-[63vw] sssm:top-[15vh] md:top-[10vh] font-bold lg:left-10 sssm:right-10 text-main3 lg:text-[10rem] sssm:text-[4rem] md:text-[7rem] xlllll:text-[14rem]">{currentIndex + 1}</h2>
+                        <h2 className="service absolute roboto-black xlllll:top-[67vh]  xlx:left-[3vw] xxl:top-[59vh] xll:top-[54vh] lgg:top-[74vh] lgg:left-[3.5vw] lg:top-[75vh] xl:top-[80vh] xl:-ml-8 xlllll:-ml-7 sssm:top-[14.7vh] sssm:left-[62.2vw] md:top-[10vh] font-bold xllll:left-[2vw] lg:left-[3vw] sssm:right-[11.5vw] ssmm:right-[10.4vw] ssm:right-[10vw] md:right-[5.5vw] md:text-[7rem] text-gray-200 lg:text-[10rem] sssm:text-[4rem] xlllll:text-[14rem] -z-1">{currentIndex + 1}</h2>
+                        <hr className="absolute xlllll:top-[82vh]  xxl:top-[73vh] xll:top-[65vh] lxl:left-[3vw] xl:top-[98vh] lgg:top-[90vh] lg:top-[95vh] sssm:top-[18vh] sssm:left-[65vw] xl:left-[2vw] xlllll:left-[1.5vw] ssmm:top-[22vh] smmm:top-[21vh] md:top-[25vh] lgg:left-[5vw] lg:left-[4vw] sssm:right-9 lg:border-2 sssm:border-1 border-gray-300 w-[7vw] xlllll:w-[6vw]"/>
+                        </div>
                     ))}
                 </ul>
+
+                
             </div>
 
             {/*Service image slider with description*/}
             <div className="w-2/3 flex items-center min-w-[350px] xl:mt-[20vh] lgg:mt-[10vh] xll:mt-[10vh] xxl:mt-[10vh] justify-center lg:mr-[12vw] relative lg:scale-140 sssm:scale-75 lg:my-8 xl:scale-170 xlllll:scale-200 lxl:scale-200 lxl:mt-[20vh] sssm:my-[-3vw] service">
                 <div className="bg-[#e8faff] w-[350px] h-[350px] p-4 xxxxxxl:p-12 rounded-lg relative xlllll:w-[450px] xlllll:h-[450px] xxxxxxl:w-[600px] xxxxxxl:h-[600px]">
                     <Image src={slides[currentIndex].img} alt="Service image" width={500} height={500} />
-                    <p className="text-black text-sm xlllll:text-xl underline font-semibold">{slides[currentIndex].title}</p>
+                    <Link href={slides[currentIndex].link} legacyBehavior>
+                        <p className="text-black text-sm xlllll:text-xl underline font-semibold cursor-pointer">{slides[currentIndex].title}</p>
+                    </Link>
                     <p className="text-gray-700 text-sm xlllll:text-lg">{slides[currentIndex].description}</p>
                     <div className="sssm:scale-140 sssm:mx-[12vw] xl:scale-100 xl:mx-0">
                         <Link href={slides[currentIndex].link} legacyBehavior>
                             <a><ReadButton /></a>
                         </Link>
-                        
                     </div>
                     {/*Next and Previous buttons*/}
                     <Image src="/Images/Services/arrow_blue.svg" width={20} height={20} quality={100} alt="next arrow" className="absolute top-1/2 xl:right-[-1.2vw] xlllll:right-[-0.8vw] md:right-[-1.8vw] sssm:right-[-3vw] sssm:scale-125 hover:sssm:scale-140 xl:scale-200 hover:xl:scale-250 cursor-pointer xxxxxxl:scale-200 xlllll:scale-250 hover:xlllll:scale-300" onClick={handleNextClick} />
